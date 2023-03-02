afaaac1fcd2d7ae85731b7e5d36af304996a20bc
agarrigu <agarrigu@protonmail.com>
2023-03-02 21:11:07 +0100

    Move a square

--- src/pixel.rs
+++ src/pixel.rs
@@ -14,7 +14,7 @@ pub unsafe fn main() {
     let white_color = XWhitePixel(display, XDefaultScreen(display));
     let window = XCreateSimpleWindow(
         display, XDefaultRootWindow(display),
-        0, 0, 200, 100, 0, black_color, white_color);
+        0, 0, 480, 320, 0, black_color, white_color);
     XSelectInput(display, window, StructureNotifyMask);
     XMapWindow(display, window);
     let graphical_context = XCreateGC(display, window, 0, NIL as *mut XGCValues );
@@ -22,17 +22,74 @@ pub unsafe fn main() {
     loop {
         let mut event: XEvent = zeroed();
         XNextEvent(display, &mut event);
+        #[allow(non_upper_case_globals)]
         match event.type_ {
-            MapNotify => break,
+            MapNotify => break, 
             _ => ()
         }
     }
-    // XDrawLine(display, window, graphical_context, 10, 60, 180, 20);
-    for i in 0..24 {
+
+    let draw_rectangle = move |a: [i32; 2], b: [i32; 2], c: [i32; 2], d: [i32; 2]| {
+        XDrawLine(display, window, graphical_context, a[0], a[1], b[0], b[1]);
+        XDrawLine(display, window, graphical_context, b[0], b[1], d[0], d[1]);
+        XDrawLine(display, window, graphical_context, d[0], d[1], c[0], c[1]);
+        XDrawLine(display, window, graphical_context, c[0], c[1], a[0], a[1]);
+    };
+
+    let mut a:  [i32; 2];
+    let mut b:  [i32; 2];
+    let mut c:  [i32; 2];
+    let mut d:  [i32; 2];
+    a = [12, 12];
+    b = [48, 12];
+    c = [12, 48];
+    d = [48, 48];
+    loop {
+
+        let mut key_event_code: u32 = 0;
+        XSelectInput(display, window, KeyPressMask | KeyReleaseMask);
+        while XPending(display) != 0 {
+            let mut key_event: XEvent = zeroed();
+            XNextEvent(display, &mut key_event);
+            #[allow(non_upper_case_globals)]
+            match key_event.type_ {
+                KeyPress => { key_event_code = key_event.key.keycode; },
+                KeyRelease => { key_event_code = key_event.key.keycode; },
+                _ => ()
+            }
+        }
+        match key_event_code {
+            111 => {
+                a[1] -= 6;
+                b[1] -= 6;
+                c[1] -= 6;
+                d[1] -= 6;
+            },
+            116 => {
+                a[1] += 6;
+                b[1] += 6;
+                c[1] += 6;
+                d[1] += 6;
+            },
+            114 => {
+                a[0] += 6;
+                b[0] += 6;
+                c[0] += 6;
+                d[0] += 6;
+            },
+            113 => {
+                a[0] -= 6;
+                b[0] -= 6;
+                c[0] -= 6;
+                d[0] -= 6;
+            },
+            _ => ()
+        }
+
         XClearWindow(display, window);
-        XDrawPoint(display, window, graphical_context, i, i);
-        sleep(Duration::from_secs(1));
+        draw_rectangle(a, b, c, d);
+        sleep(Duration::from_millis(50));
         XFlush(display);
     }
-    sleep(Duration::from_secs(10));
 }
+
